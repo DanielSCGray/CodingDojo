40aea668667fcdaa3b1cf05e32b2f3803b549c6e
Daniel Gray <danielscgray@gmail.com>
2023-07-18 14:13:34 -0700

    7/18 afternoon assignments

---
+++ Web_Fundamentals/JS/Gray_Daniel_ArrayChallenges.js
@@ -0,0 +1,30 @@
+function alwaysHungry(array) {
+    var foodCount = 0
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === 'food') {
+            console.log('yummy')
+            foodCount ++
+        }
+        
+    }
+    if (foodCount === 0) {
+        console.log("I'm hungry")
+    }
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
