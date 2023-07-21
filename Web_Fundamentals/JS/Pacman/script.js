0477e9fc338ec57ffe94af6a2d126e46f32354cd
Daniel Gray <danielscgray@gmail.com>
2023-07-21 12:55:08 -0700

    pacman project with html

---
+++ Web_Fundamentals/JS/Pacman/script.js
@@ -0,0 +1,31 @@
+var world = [
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+]
+
+function displayWorld() {
+    var output = ''
+    for(var i =0; i<world.length; i++){
+        output += "\n<div class='row'>"
+        for(var j =0; j<world[i].length; j++) {
+            if(world[i][j] === 2) {
+                output += "\n\t<div class='brick'></div>"
+            }
+            if(world[i][j] === 1) {
+                output += "\n\t<div class='coin'></div>"
+            }
+        }
+        output += "\n</div>"
+    }
+    console.log(output)
+}
+
+displayWorld()
