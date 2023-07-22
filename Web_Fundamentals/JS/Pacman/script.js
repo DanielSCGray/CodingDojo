4e16a0c138f14f4526fc8b334d08c3911bb45967
Daniel Gray <danielscgray@gmail.com>
2023-07-22 15:35:44 -0700

    added colision detection, responsive rotation

--- Web_Fundamentals/JS/Pacman/script.js
+++ Web_Fundamentals/JS/Pacman/script.js
@@ -1,6 +1,6 @@
 var world = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 0, 1, 0, 1, 1, 1, 1, 1, 2],
     [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
     [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
     [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
@@ -10,6 +10,11 @@ var world = [
     [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ]
+
+var pacman = {
+    x: 1,
+    y: 1
+}
 
 function displayWorld() {
     var output = ''
@@ -22,10 +27,60 @@ function displayWorld() {
             if(world[i][j] === 1) {
                 output += "<div class='coin'></div>"
             }
+            if(world[i][j] === 0){
+                output += "<div class='empty'></div>"
+            }
         }
         output += "\n</div>"
     }
     document.getElementById('world').innerHTML = output;
 }
 
+function displayPacman(){
+    document.getElementById('pacman').style.top = pacman.y*40+'px';
+    document.getElementById('pacman').style.left = pacman.x*40+'px';
+}
+
 displayWorld()
+displayPacman()
+
+document.onkeydown = function(e){
+    console.log(e.key)
+    if (e.key === 'ArrowDown') {
+        document.getElementById('pacman').style.transform = 'rotate(90deg)'
+        pacman.y++;
+        if(world[pacman.y][pacman.x] === 2){
+            pacman.y--
+        }
+    }
+    else if (e.key === 'ArrowUp'){
+        document.getElementById('pacman').style.transform = 'rotate(270deg)'
+        pacman.y--
+        if(world[pacman.y][pacman.x] === 2){
+            pacman.y++
+        }
+    }
+    else if (e.key === 'ArrowRight'){
+        document.getElementById('pacman').style.transform = 'rotate(0deg)'
+        pacman.x++
+        if(world[pacman.y][pacman.x] === 2){
+            pacman.x--
+        }
+    }
+    else if (e.key === 'ArrowLeft'){
+        document.getElementById('pacman').style.transform = 'rotate(180deg)'
+        pacman.x--
+        if(world[pacman.y][pacman.x] === 2){
+            pacman.x++
+        }
+    }
+
+    if (world[pacman.y][pacman.x] === 1){
+        world[pacman.y][pacman.x] = 0;
+        displayWorld();
+    }
+    // console.log(world[pacman.y][pacman.x])
+    // console.log(pacman.y)
+
+    displayPacman();
+}
