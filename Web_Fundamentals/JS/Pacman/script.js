e9e88d5ff04712e26092076ebb6ab2e6fd642675
Daniel Gray <danielscgray@gmail.com>
2023-07-23 15:41:53 -0700

    created ghosts, built movement AI

--- Web_Fundamentals/JS/Pacman/script.js
+++ Web_Fundamentals/JS/Pacman/script.js
@@ -1,19 +1,34 @@
 var world = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 0, 1, 0, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 3, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 1, 2],
+    [2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2],
+    [2, 1, 1, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2],
+    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2],
+    [2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 1, 2],
+    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2],
+    [2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 1, 1, 2],
+    [2, 1, 2, 1, 2, 1, 2, 1, 2, 0, 2, 1, 1, 1, 1, 2, 1, 2, 1, 2],
+    [2, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 1, 2],
+    [2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2],
+    [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2],
+    [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2],
+    [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 3, 1, 1, 2, 1, 2, 1, 2, 1, 2],
+    [2, 1, 1, 0, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+    [2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2],
+    [2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2],
+    [2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+
 ]
 
 var pacman = {
     x: 1,
     y: 1
+}
+var redGhost = {
+    x: 9,
+    y: 9
 }
 
 function displayWorld() {
@@ -43,6 +58,12 @@ function displayPacman(){
     document.getElementById('pacman').style.top = pacman.y*40+'px';
     document.getElementById('pacman').style.left = pacman.x*40+'px';
 }
+
+function displayGhost(){
+    document.getElementById('redghost').style.top = redGhost.y*40+'px';
+    document.getElementById('redghost').style.left = redGhost.x*40+'px';
+}
+
 var score = document.getElementById('score')
 function updateScore(val){
     var newScore = Number(score.innerText) + val
@@ -52,8 +73,126 @@ function updateScore(val){
 
 displayWorld()
 displayPacman()
+displayGhost()
 
+// GHOST MOVEMENT 
+function ghostMoveRandom(){
+    var directionArray = ['down', 'up', 'right', 'left']
+    var direction = directionArray[Math.floor(Math.random()*4)]
+    if (direction === 'down') {
+        redGhost.y++;
+        if(world[redGhost.y][redGhost.x] === 2){
+            redGhost.y--
+        }
+    }
+    if (direction === 'up') {
+        redGhost.y--;
+        if(world[redGhost.y][redGhost.x] === 2){
+            redGhost.y++
+        }
+    }
+    if (direction === 'right') {
+        redGhost.x++;
+        if(world[redGhost.y][redGhost.x] === 2){
+            redGhost.x--
+        }
+    }
+    if (direction === 'left') {
+        redGhost.x--;
+        if(world[redGhost.y][redGhost.x] === 2){
+            redGhost.x++
+        }
+    }
+    console.log('ghost moves ' + direction)
+}
 
+function buildSmartDirections(ghost){
+    var smartDirectionArray = []
+    var xAxis = pacman.x - ghost.x
+    var yAxis = pacman.y - ghost.y
+    while (smartDirectionArray.length < 4) {
+        if (Math.abs(xAxis) < Math.abs(yAxis)){
+            if (yAxis > 0){
+                smartDirectionArray.push('down')
+            }
+            else {
+                smartDirectionArray.push('up')
+            }
+            if (xAxis > 0){
+                smartDirectionArray.push('right')
+            }
+            else {
+                smartDirectionArray.push('left')
+            }
+            if (smartDirectionArray[0] === 'down'){
+                smartDirectionArray.push('up')
+            }
+            else {
+                smartDirectionArray.push('down')
+            }
+            if (smartDirectionArray[1] === 'right'){
+                smartDirectionArray.push('left')
+            }
+            else {
+                smartDirectionArray.push('right')
+            }
+        }
+        else {
+            if (xAxis > 0){
+                smartDirectionArray.push('right')
+            }
+            else {
+                smartDirectionArray.push('left')
+            }
+            if (yAxis > 0){
+                smartDirectionArray.push('down')
+            }
+            else {
+                smartDirectionArray.push('up')
+            }
+            if (smartDirectionArray[0] === 'right'){
+                smartDirectionArray.push('left')
+            }
+            else {
+                smartDirectionArray.push('right')
+            }
+            if (smartDirectionArray[1] === 'down'){
+                smartDirectionArray.push('up')
+            }
+            else {
+                smartDirectionArray.push('down')
+            }
+        }
+    }
+    console.log(smartDirectionArray)
+    return smartDirectionArray;
+}
+function smartGhostMove(ghost){
+    var directionArr = buildSmartDirections(ghost);
+    for (i=0; i<directionArr.length;i++){
+        var smartDirection = directionArr[i]
+        if(smartDirection === 'down' && world[ghost.y+1][ghost.x] !== 2){
+            ghost.y++;
+            console.log('SMART ghost moves ' + smartDirection)
+            return
+        }
+        else if(smartDirection === 'up' && world[ghost.y-1][ghost.x] !== 2){
+            ghost.y--;
+            console.log('SMART ghost moves ' + smartDirection)
+            return
+        }
+        else if(smartDirection === 'right' && world[ghost.y][ghost.x+1] !== 2){
+            ghost.x++;
+            console.log('SMART ghost moves ' + smartDirection)
+            return
+        }
+        else if(smartDirection === 'left' && world[ghost.y][ghost.x-1] !== 2){
+            ghost.x--;
+            console.log('SMART ghost moves ' + smartDirection)
+            return
+        }
+    }
+}
 
 document.onkeydown = function(e){
     console.log(e.key)
@@ -98,6 +237,11 @@ document.onkeydown = function(e){
     }
     // console.log(world[pacman.y][pacman.x])
     // console.log(pacman.y)
-
+    // ghostMoveRandom();
+    smartGhostMove(redGhost);
+    displayGhost();
     displayPacman();
+
 }
+
+
